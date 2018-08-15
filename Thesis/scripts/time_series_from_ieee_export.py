a3a3628de429e13d958ef3d1b40911199a803839
Salih Komsucuk <s.komsucuk@outlook.de>
2018-08-15 16:40:52 +0200

    ignore csv errors at import

--- Thesis/scripts/time_series_from_ieee_export.py
+++ Thesis/scripts/time_series_from_ieee_export.py
@@ -10,37 +10,38 @@ os.chdir(data_dir)
 data_file_pattern_acm = glob.glob(data_dir + r'./ieee*ART.csv')
 data_file_pattern_acm.extend(glob.glob(data_dir + r'./ieee*PROC.csv'))
 
-# print data_file_pattern_acm
+#print(data_file_pattern_acm)
 
 for acm_file in data_file_pattern_acm:
-	dict_year_record = {}
-	lst_years = []
+    print(acm_file)
+    dict_year_record = {}
+    lst_years = []
 
-	with open(acm_file, 'r') as csv_file_raw:
-		data_file_raw = csv.reader(csv_file_raw)
-		for i, line in enumerate(data_file_raw):
-			if line[5][0].isdigit():
-				lst_years.append(line[5])
-			# print str(i) + ": " + line[5]
-		dict_year_record = dict(Counter(lst_years))
-		# print dict_year_record
+    with open(acm_file, 'r', errors='ignore') as csv_file_raw:
+        data_file_raw = csv.reader(csv_file_raw)
+        for i, line in enumerate(data_file_raw):
+            if line[5][0].isdigit():
+                lst_years.append(line[5])
+            # print str(i) + ": " + line[5]
+        dict_year_record = dict(Counter(lst_years))
+        # print dict_year_record
 
-	lst_year_record = sorted(dict_year_record.keys())
+    lst_year_record = sorted(dict_year_record.keys())
 
-	for i,j in enumerate(range(1, len(lst_year_record))):
-		# print("a", lst_year_record[j], lst_year_record[i])
-		if int(lst_year_record[j]) - int(lst_year_record[i]) > 1:
-			for k in range(int(lst_year_record[j]) - int(lst_year_record[i]) - 1):
-				# print("b", str(int(lst_year_record[j]) - k - 1))
-				dict_year_record[str(int(lst_year_record[j]) - k - 1)] = "0"
+    for i,j in enumerate(range(1, len(lst_year_record))):
+        # print("a", lst_year_record[j], lst_year_record[i])
+        if int(lst_year_record[j]) - int(lst_year_record[i]) > 1:
+            for k in range(int(lst_year_record[j]) - int(lst_year_record[i]) - 1):
+                # print("b", str(int(lst_year_record[j]) - k - 1))
+                dict_year_record[str(int(lst_year_record[j]) - k - 1)] = "0"
 
-	# for k in sorted(dict_year_record.keys()):
-		# print(k, dict_year_record[k])
+    # for k in sorted(dict_year_record.keys()):
+        # print(k, dict_year_record[k])
 
-	with open(acm_file.lower().replace(".csv", "_ts.csv"), 'w+') as csv_output:
-		fieldnames = ['years', 'records']
-		writer = csv.DictWriter(csv_output, fieldnames=fieldnames, lineterminator='\n')
-		
-		writer.writeheader()
-		for k in sorted(dict_year_record.keys()):
-			writer.writerow({"%s" % (fieldnames[0]): "%s" % (k), "%s" % (fieldnames[1]): "%s" % (dict_year_record[k])})
+    with open(acm_file.lower().replace(".csv", "_ts.csv"), 'w+') as csv_output:
+        fieldnames = ['years', 'records']
+        writer = csv.DictWriter(csv_output, fieldnames=fieldnames, lineterminator='\n')
+        
+        writer.writeheader()
+        for k in sorted(dict_year_record.keys()):
+            writer.writerow({"%s" % (fieldnames[0]): "%s" % (k), "%s" % (fieldnames[1]): "%s" % (dict_year_record[k])})
--- Thesis/scripts/time_series_from_wos_export.py
+++ Thesis/scripts/time_series_from_wos_export.py
@@ -9,31 +9,31 @@ data_file_pattern_wos = glob.glob(data_dir + r'./wos*ART.txt')
 data_file_pattern_wos.extend(glob.glob(data_dir + r'./wos*PROC.txt'))
 
 for wos_file in data_file_pattern_wos:
-	dict_year_record = {}
+    dict_year_record = {}
 
-	with open(wos_file, 'r') as data_file_raw:
-		line_input = iter(data_file_raw)
-		for li in line_input:
-			if li[0].isdigit():
-				li_split = li.split('\t')
-				dict_year_record[li_split[0]] = li_split[1]
+    with open(wos_file, 'r', errors='ignore') as data_file_raw:
+        line_input = iter(data_file_raw)
+        for li in line_input:
+            if li[0].isdigit():
+                li_split = li.split('\t')
+                dict_year_record[li_split[0]] = li_split[1]
 
-	lst_year_record = sorted(dict_year_record.keys())
+    lst_year_record = sorted(dict_year_record.keys())
 
-	for i,j in enumerate(range(1, len(lst_year_record))):
-	#	print("a", lst_year_record[j], lst_year_record[i])
-		if int(lst_year_record[j]) - int(lst_year_record[i]) > 1:
-			for k in range(int(lst_year_record[j]) - int(lst_year_record[i]) - 1):
-	#			print("b", int(lst_year_record[j]) - k - 1)
-				dict_year_record[str(int(lst_year_record[j]) - k - 1)] = "0"
+    for i,j in enumerate(range(1, len(lst_year_record))):
+    #   print("a", lst_year_record[j], lst_year_record[i])
+        if int(lst_year_record[j]) - int(lst_year_record[i]) > 1:
+            for k in range(int(lst_year_record[j]) - int(lst_year_record[i]) - 1):
+    #           print("b", int(lst_year_record[j]) - k - 1)
+                dict_year_record[str(int(lst_year_record[j]) - k - 1)] = "0"
 
-	# for k in sorted(dict_year_record.keys()):
-		# print(k + ": " + dict_year_record[k])
+    # for k in sorted(dict_year_record.keys()):
+        # print(k + ": " + dict_year_record[k])
 
-	with open(wos_file.lower().replace(".txt", "_ts.csv"), 'w+') as data_file_ts:
-		data_file_ts.write("years,records\n")
-		for k in sorted(dict_year_record.keys()):
-			data_file_ts.write("%s,%s\n" % (k, dict_year_record[k]))
+    with open(wos_file.lower().replace(".txt", "_ts.csv"), 'w+') as data_file_ts:
+        data_file_ts.write("years,records\n")
+        for k in sorted(dict_year_record.keys()):
+            data_file_ts.write("%s,%s\n" % (k, dict_year_record[k]))
 
-	data_file_raw.close()
-	data_file_ts.close()
+    data_file_raw.close()
+    data_file_ts.close()
