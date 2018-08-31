0da0c971a0e1d3c36acff58a495b3e1b205f015a
Salih Komsucuk <s.komsucuk@outlook.de>
2018-09-01 01:31:42 +0200

    started section 6

--- Thesis/scripts/desc.R
+++ Thesis/scripts/desc.R
@@ -3,48 +3,43 @@ all_data_raw <- read.csv("D:/dev/fom/Thesis/data/structured/time_series_all.csv"
 str(all_data_raw)
 
 START <- 2010
-SCHLUSS <- 2018
+END <- 2018
 
-all_data <- all_data_raw[which(all_data_raw$years >= START & all_data_raw$years <= SCHLUSS),]
+all_data <- all_data_raw[which(all_data_raw$years >= START & all_data_raw$years <= END),]
 
-t <- 1
 for(technology in unique(all_data[,"technology"])){
-#  technology <- "ML"
 
   rows_technology <- which(all_data[,"technology"] == technology)
   technologies <- all_data[rows_technology,]
   
   par(ask = TRUE)
-  plot(x = technologies[,"years"], y = technologies[,"records"], main = technology, xlab = "Veröffentlichungsjahr", ylab = "Anzahl Publikationen", type = "n", ylim = c(min(technologies[,"records"]), max(technologies[,"records"])+max(teildatensatz[, "records"])/10), xlim = c(min(technologies[,"years"]), max(technologies[,"years"])))
-  #text(x = unique(technologies[,"years"]), y = -10, srt = 90, labels = unique(technologies[,"years"]))
-  legendentext <- character(0)
+  plot(x = technologies[,"years"], y = technologies[,"records"], main = technology, xlab = "Veröffentlichungsjahr", ylab = "Anzahl Publikationen", type = "n", ylim = c(min(technologies[,"records"]), max(technologies[,"records"])), xlim = c(min(technologies[,"years"]), max(technologies[,"years"])))
+  legend_text <- character(0)
+  # grid()
   s <- 1
+  p <- 1
   for(search_engine in unique(technologies[,"search_engine"])){
+    s <- s + 1
     rows_search <- which(technologies[,"search_engine"] == search_engine)
     search_engines <- technologies[rows_search,]
-    legendentext <- c(legendentext, paste(search_engine, sep = ""))
-    #SORTIERUNG <- sort(search_engines[, "years"], index.return = TRUE)$ix
-    for(p in unique(search_engines[,"publication"])){
-      rows_pub <- which(search_engines[,"publication"] == p)
+    legend_text <- c(legend_text, paste(search_engine, sep = ""))
+    for(publication in unique(search_engines[,"publication"])){
+      p <- p + 1
+      rows_pub <- which(search_engines[,"publication"] == publication)
       publications <- search_engines[rows_pub,]
-      
-      #mod_lm <- lm(publications$records ~ publications$years) 
-      #summary(mod_lm)
-      
+
       if(nrow(publications) == 1){
         publications <- rbind(publications[1,], publications, publications[1,])
         publications[1,"years"] <- publications[1,"years"] - 0.2
         publications[3,"years"] <- publications[3,"years"] + 0.2
-        points(x = publications[,"years"], y = publications[,"records"], type = "l", col = t, lty = s, lwd = s)
-        points(x = publications[2,"years"], y = publications[2,"records"], type = "p", col = t, pch = ifelse(publications[2,"publication"] == "A", 20, 2), cex = ifelse(publications[2,"publication"] == "A", 1, 1.5))
+        points(x = publications[,"years"], y = publications[,"records"], type = "l", col = p, lty = s)
+        points(x = publications[2,"years"], y = publications[2,"records"], type = "p", col = p, pch = ifelse(publications[2,"publication"] == "A", 20, 2), cex = ifelse(publications[2,"publication"] == "A", 1, 1.5))
       }else{
-        points(x = publications[,"years"], y = publications[,"records"], type = "l", col = t, lty = s, lwd = s)
-        points(x = publications[,"years"], y = publications[,"records"], type = "p", col = t, pch = ifelse(publications[,"publication"] == "A", 20, 2), cex = ifelse(publications[,"publication"] == "A", 1, 1.5))
+        points(x = publications[,"years"], y = publications[,"records"], type = "l", col = p, lty = s)
+        points(x = publications[,"years"], y = publications[,"records"], type = "p", col = p, pch = ifelse(publications[,"publication"] == "A", 20, 2), cex = ifelse(publications[,"publication"] == "A", 1, 1.5))
       }
     }
-    s <- s + 1
   }
-  legend("topleft", legend = legendentext, bty = "n", lwd = 1:length(legendentext), lty = 1:length(legendentext))
+  legend("topleft", legend = legend_text, bty = "n", lwd = 1:length(legend_text), lty = 1:length(legend_text))
   legend("topright", legend = c("A", "KB"), bty = "n", pch = c(20, 2))
-  t <- t + 1
 }
